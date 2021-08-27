d1bc5d385178edfda3b7641fd2ece62867bbce55
Jose Claudio <jccardosors@gmail.com>
2021-08-27 18:35:23 -0300

    Add project files.

---
+++ AppCliente/Models/Tutor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCliente.Models
+{
+    public class Tutor
+    {
+        public int Id { get; set; }
+        public int InstitucionalizadoId { get; set; }
+        public int Cpf { get; set; }
+        public int CI { get; set; }
+        public int EstadoCivil { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Nome { get; set; }
+        public int Sexo { get; set; }
+        public int Idade { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime DataInclusao { get; set; }
+        public DateTime DataAlteracao { get; set; }
+
+    }
+}
---
+++ AppCliente/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCliente.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public int Tipo { get; set; }
+
+        public string NomeUsuario { get; set; }
+
+        public string Senha { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public DateTime DataInclusao { get; set; }
+
+        public DateTime DataAlteracao { get; set; }
+
+    }
+}
